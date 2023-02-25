16a0b37a980c48af49d0d57aee9f95cbc9b54be1
enesbee <nsb10032@gmail.com>
2023-02-26 01:43:18 +0900

    타이핑 툴 스타일 완성

--- src/pages/TypingPage.tsx
+++ src/pages/TypingPage.tsx
@@ -1,105 +1,122 @@
-import React, {useEffect, useState} from 'react';
-import {useNavigate} from 'react-router-dom';
-import {Keyword, TypingSection, TypingSectionInner } from './styles/Typing.style';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+	Keyword,
+	TypingBox,
+	TypingBoxContent,
+	TypingBoxTaskBar,
+	TypingBoxTaskBarButtons,
+	TypingBoxTaskBarTitle,
+	TypingEditor,
+	TypingEditorBox,
+	TypingEditorContents,
+	TypingEditorContentsTop,
+	TypingEditorSplitView,
+	TypingSection,
+	TypingSectionInner,
+} from './styles/Typing.style';
 
 const TypingPage = () => {
-    const [text, setText] = useState("");
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [errorCount, setErrorCount] = useState(0);
-    const [startTime, setStartTime] = useState<string>('');
-    const [endTime, setEndTime] = useState<string>('');
-    const navigate = useNavigate();
+	const [text, setText] = useState('');
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const [errorCount, setErrorCount] = useState(0);
+	const [startTime, setStartTime] = useState<string>('');
+	const [endTime, setEndTime] = useState<string>('');
+	const navigate = useNavigate();
 
-    const mockData = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+	const mockData = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
 
-    useEffect(() => {
-        document.addEventListener("keydown", handleKeyDown);
-        return () => {
-            document.removeEventListener("keydown", handleKeyDown);
-        };
-    }, [currentIndex]);
+	useEffect(() => {
+		document.addEventListener('keydown', handleKeyDown);
+		return () => {
+			document.removeEventListener('keydown', handleKeyDown);
+		};
+	}, [currentIndex]);
 
-    const handleKeyDown = (event: KeyboardEvent) => {
-        const { key } = event;
-        const currentChar = mockData[currentIndex];
-        // 이상한 키를 눌렀을 때
-        if (key !== currentChar) {
-            if (key === 'Enter') {
-                // 엔터 키 라면(결과페이지로 이동)
-                localStorage.setItem("startTime", startTime);
-                localStorage.setItem("endTime", endTime);
-                navigate('/result', {
-                    state: {
-                        dataLength: mockData.length
-                    }
-                });
-            } else {
-                //아니라면
-                let newErrorCount = errorCount + 1;
-                setErrorCount(newErrorCount);
-            }
-        } else {
-            let newIndex = currentIndex + 1;
-            setCurrentIndex(newIndex);
-            if (newIndex === 1) {
-                setStartTime(Date.now().toString());
-            }
-            if (newIndex === mockData.length) {
-                setEndTime(Date.now().toString());
-            }
-        }
+	const handleKeyDown = (event: KeyboardEvent) => {
+		const { key } = event;
+		const currentChar = mockData[currentIndex];
+		// 이상한 키를 눌렀을 때
+		if (key !== currentChar) {
+			if (key === 'Enter') {
+				// 엔터 키 라면(결과페이지로 이동)
+				localStorage.setItem('startTime', startTime);
+				localStorage.setItem('endTime', endTime);
+				navigate('/result', {
+					state: {
+						dataLength: mockData.length,
+					},
+				});
+			} else {
+				//아니라면
+				let newErrorCount = errorCount + 1;
+				setErrorCount(newErrorCount);
+			}
+		} else {
+			let newIndex = currentIndex + 1;
+			setCurrentIndex(newIndex);
+			if (newIndex === 1) {
+				setStartTime(Date.now().toString());
+			}
+			if (newIndex === mockData.length) {
+				setEndTime(Date.now().toString());
+			}
+		}
+	};
 
-    };
-
-    return (
-        <TypingSection>
-            <TypingSectionInner>
-            <div>
-                {mockData.split("").map((char, index) => (
-                    <Keyword
-                        key={index}
-                        className={`${index < currentIndex ? 'isComplete' : '' } ${ index === currentIndex ? 'isActive' : ''}`}
-                    >
-            {char}
-          </Keyword>
-                ))}
-            </div>
-            </TypingSectionInner>
-
-            <div className="window terminal ui-draggable ui-draggable-handle"
-                 style="position: relative; display: block; left: -375px; top: -233px;">
-                <div className="window__taskbar">
-                    <div className="window__taskbar--actions">
-                        <button className="close"></button>
-                        <button className="backfull"></button>
-                        <button className="full"></button>
-                    </div>
-                    <div className="window__taskbar--content">
-                        <h2>Terminal</h2>
-                    </div>
-                </div>
-                <div className="terminal_content">
-                    <p>Last Login : Yesterday on console</p>
-
-                    <!--    Oh My Zsh bash      -->
-                    <div className="terminal_line">
-                        <p>
-                            <span className="color_green">➜</span>&nbsp;&nbsp;
-                            <span className="color_blue">~</span>&nbsp;
-                            <span contentEditable="true" className="cursor"></span>
-                        </p>
-                    </div>
-
-                    <!--    MacOs Default bash                                 -->
-                    <!--          <div class="terminal_line">                            -->
-                    <!--            <p>MacBook-Pro:~ root$&nbsp;                         -->
-                    <!--              <span contenteditable="true" class="cursor"></span>-->
-                    <!--            </p>                                                 -->
-                    <!--          </div>                                                 -->
-                </div>
-            </div>
-        </TypingSection>
-    );
+	return (
+		<TypingSection>
+			<TypingSectionInner>
+				<TypingBox>
+					<TypingBoxTaskBar>
+						<TypingBoxTaskBarButtons>
+							<span></span>
+							<span></span>
+							<span></span>
+						</TypingBoxTaskBarButtons>
+						<TypingBoxTaskBarTitle>
+							<h2>Visual Studio Code</h2>
+						</TypingBoxTaskBarTitle>
+					</TypingBoxTaskBar>
+					<TypingBoxContent>
+						<TypingEditorBox>
+							<TypingEditorSplitView>
+								<div className='codicon codicon-menu'></div>
+								<div>
+									<div className='codicon codicon-files'></div>
+									<div className='codicon codicon-search'></div>
+									<div className='codicon codicon-source-control'></div>
+									<div className='codicon codicon-debug-alt'></div>
+									<div className='codicon codicon-extensions'></div>
+								</div>
+								<div>
+									<div className='codicon codicon-account'></div>
+									<div className='codicon codicon-settings-gear'></div>
+								</div>
+							</TypingEditorSplitView>
+							<TypingEditorContents>
+								<TypingEditorContentsTop>
+									<span>App.js</span>
+									<span className='codicon codicon-close'></span>
+								</TypingEditorContentsTop>
+								<TypingEditor>
+                                        {mockData.split('').map((char, index) => (
+                                            <Keyword
+                                                key={index}
+                                                className={`${index < currentIndex ? 'isComplete' : ''} ${
+                                                    index === currentIndex ? 'isActive' : ''
+                                                }`}>
+                                                {char}
+                                            </Keyword>
+                                        ))}
+                                </TypingEditor>
+							</TypingEditorContents>
+						</TypingEditorBox>
+					</TypingBoxContent>
+				</TypingBox>
+			</TypingSectionInner>
+		</TypingSection>
+	);
 };
 
 export default TypingPage;
--- src/pages/styles/Typing.style.ts
+++ src/pages/styles/Typing.style.ts
@@ -1,30 +1,175 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const TypingSection = styled.section`
-  display: flex;
-  padding: 20px;
-  width: 100%;
+	display: flex;
+	padding: 20px;
+	width: 100%;
 `;
 export const TypingSectionInner = styled.div`
-  display: inherit;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  min-height:70vh;
-  flex-direction: column;
-  font-family: 'D2Coding', sans-serif;
+	display: inherit;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	min-height: 70vh;
+	flex-direction: column;
 `;
 
 export const Keyword = styled.span`
-  color:#bbb;
-  font-size: 28px;
-  font-weight: normal;
+	color: #bbb;
+	font-size: 16px;
+	font-weight: normal;
 
-    &.isActive {
-      font-size: 32px;
-      font-weight: bold;
+	&.isActive {
+		font-size: 20px;
+		font-weight: bold;
+	}
+	&.isComplete {
+		color: #888;
+	}
+`;
+
+export const TypingBox = styled.div`
+	cursor: auto;
+	margin: 0;
+	padding: 0;
+	outline: 0;
+	list-style: none;
+	min-width: 50%;
+	max-width: 80%;
+	min-height: 60vh;
+	overflow: hidden;
+	border-radius: 1rem;
+	background: transparent;
+	box-shadow: 0 20px 30px rgba(0, 0, 0, 0.5);
+	border: 1px solid #ffffff34;
+	animation: zoom-out 0.3s;
+	z-index: 10;
+	touch-action: none;
+	transition: 110ms;
+	position: relative;
+	display: block;
+`;
+
+export const TypingBoxTaskBar = styled.div`
+  height: 35px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 16px;
+  background-color: rgb(252, 255, 251);
+`;
+
+export const TypingBoxTaskBarButtons = styled.div`
+	display: flex;
+	align-items: center;
+
+	span {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 0.8rem;
+		height: 0.8rem;
+		padding: 8px;
+		border-radius: 50%;
+		transition: 0.4s;
+
+		&:nth-child(1) {
+			background-color: #ff5f56;
+			box-shadow: 0 0 0 0.5px #e0443e;
+		}
+		&:nth-child(2) {
+			background-color: #ffbd2e;
+			box-shadow: 0 0 0 0.5px #dea123;
+		}
+		&:nth-child(3) {
+			background-color: #27c93f;
+			box-shadow: 0 0 0 0.5px #1aab29;
+		}
+		&:not(:last-child) {
+			margin-right: 8px;
+		}
+	}
+`;
+
+export const TypingBoxTaskBarTitle = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+export const TypingBoxContent = styled.div`
+	width: 100%;
+	height: 91%;
+	overflow-y: auto;
+	background-color: rgba(5, 5, 99, 0.63);
+	backdrop-filter: blur(12px);
+	color: rgb(235, 235, 235);
+	font-family: 'D2Coding', sans-serif;
+`;
+
+export const TypingEditorBox = styled.div`
+  position: relative;
+  display: flex;
+  width:100%; height: 100%;
+  
+`;
+export const TypingEditorSplitView = styled.div`
+  width:48px; height:100%;
+  flex-shrink: 0;
+  background-color:rgb(51, 51, 51);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  
+  > div:not(:first-child) {
+    display: flex;
+    flex-direction: column;
+    
+    &:nth-child(2) {margin-bottom:auto;}
+
+    > div {
+      color:rgba(255, 255, 255, 0.4);
+      display: flex!important;
+      align-items: center;
+      justify-content: center;
+      box-sizing: border-box;
+      white-space: nowrap;
+      outline: 0;
+      font-size: 20px;
+      height:36px;
     }
-    &.isComplete {
-      color:#888;
-    }
+  }
+  .codicon-menu {display:inherit; height:35px; font-size: 16px; justify-content:center; align-items: center}
+`;
+
+export const TypingEditorContents = styled.div`
+  flex-grow:1;
+  width:100%; height: 100%;
+  display: flex;
+  background-color: rgb(37, 37, 38);
+  flex-direction: column;
+  overflow-x: hidden;
+`;
+
+export const TypingEditorContentsTop = styled.div`
+  display: flex;
+  height:35px;
+  width: 120px;
+  min-width: fit-content;
+  flex-shrink: 0;
+  background-color: rgb(30, 30, 30);
+  justify-content: space-between;
+  
+  span {
+    display:inherit!important;
+    align-items: center;
+    min-width:16px; line-height: 16px;
+    margin:0 6px;
+    font-size:13px;
+  }
+`;
+export const TypingEditor = styled.div`
+  width: 100%;
+  margin:1rem 1rem 1rem 2rem;
 `;
